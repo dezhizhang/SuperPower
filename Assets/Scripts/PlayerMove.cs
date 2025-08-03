ee0bb2e97deaf83738b8f265130540941e52b0e9
dezhizhang <1541609448@qq.com>
2025-08-03 17:56:19 +0800

    feat: 更新条件

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 玩家移动
+/// </summary>
+public class PlayerMove : MonoBehaviour
+{
+    public GameObject player;
+    
+    
+}
